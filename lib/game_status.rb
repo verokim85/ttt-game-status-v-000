0f846b7bacaed6466d871686daefe984ad753190
Veronica Kim <vero.a.kim@gmail.com>
2017-12-22 23:20:23 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -34,8 +34,8 @@ end
 
 
 def full?(board)
-  board.each {|i|}
-  index = i
+  board.each {|num|}
+  index = num
   if position_taken?(board, index) == "X" || position_taken?(board, index) == "O"
      return true
   end
